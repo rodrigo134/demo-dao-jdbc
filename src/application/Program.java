28fe174aed4c1303bee87fdf0f0f1bc7f0ea7352
Rodrigo de Oliveira <rodrigooliveirareis98@gmail.com>
2025-07-13 12:51:55 -0300

    seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,21 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
 		
-		Department dp= new Department(12, "books");
-		System.out.println(dp);
+		Department obj= new Department(12, "books");
+		System.out.println(obj);
+		
+		Seller seller = new Seller(21,"bob","bob@gmail.com",new Date(),3000.0,obj);
+		
+		System.out.println(seller);
 		
 		
 		
